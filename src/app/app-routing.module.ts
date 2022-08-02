f02a5baad3ff00326c3b3ca583688238d41b59dc
Viswanadh Naidu Kimidi <viswanadh.kimidi@valuemomentum.com>
2022-08-02 12:30:19 +0530

    commiting my workspace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'addtask',component:AddTaskComponent},
+  {path:'listtask',component:ListTasksComponent},
+  {path:'edittask',component:EdittaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { HeaderComponent } from './header/header.component';
+import { EdittaskComponent } from './edittask/edittask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddTaskComponent,
+    ForgotPasswordComponent,
+    ListUsersComponent,
+    ViewProfileComponent,
+    ListTasksComponent,
+    ChangePasswordComponent,
+    HeaderComponent,
+    EdittaskComponent
   ],
   imports: [
     BrowserModule,
